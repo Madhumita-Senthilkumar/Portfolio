b05fcb69c007feefdc4d4dbfe9ebb1bb7a0e5877
Madhumita <20bit037@mcet.in>
2023-04-25 16:15:44 +0530

    first commit

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,49 +1,12 @@
-let splash=document.querySelector('.splash');
-let splash1=document.querySelector('.splash-1');
-let splashspan=document.querySelectorAll('.h1');
-let splashspan2=document.querySelectorAll('.h2');
-window.addEventListener('DOMContentLoaded',()=>
+function validate()
 {
-    setTimeout(()=>
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var country=document.getElementById("country").value;
+    var subject=document.getElementById("subject").value;
+    if(name=="" && email=="" && country=="" && subject=="")
     {
-            splashspan.forEach((span,idx)=>{
-                setTimeout(()=>{
-                        span.classList.add('active');
-                    },(idx +1)*400)
-                   }   );
-                setTimeout(()=>{
-                    splashspan.forEach((span,idx)=>{
-                        setTimeout(()=>{
-                                span.classList.remove('active');
-                                span.classList.add('fade');
-                            },(idx +1)*50)
-                           }   );
-                    },2000
-                    
-                );
-        
-            }
-            )
-            splashspan2.forEach((span,idx)=>{
-                setTimeout(()=>{
-                        span.classList.add('active');
-                    },(idx +1)*400)
-                   }   );
-                setTimeout(()=>{
-                    splashspan2.forEach((span,idx)=>{
-                        setTimeout(()=>{
-                                span.classList.remove('active');
-                                span.classList.add('fade');
-                            },(idx +1)*50)
-                           }   );
-                    },2000);
-                    setTimeout(()=>
-                    {
-                        splash.getElementsByClassName.top='-100vh';
-
-                    },2300
-                    )
+        alert("All fields are required");
+        return false;
+    }
 }
-)
-
-    
